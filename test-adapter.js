9e368756fa5bb431c94e7b6914f6feea8ce77695
songletli <songlet.li@wisetechglobal.com>
2025-11-07 16:26:00 +0800

    default adapter

---
+++ test-adapter.js
@@ -0,0 +1,25 @@
+"use strict";
+
+module.exports = {
+  resolved: function(value) {
+    return Promise.resolve(value);
+  },
+  
+  rejected: function(reason) {
+    return Promise.reject(reason);
+  },
+  
+  deferred: function() {
+    var resolve, reject;
+    var promise = new Promise(function(res, rej) {
+      resolve = res;
+      reject = rej;
+    });
+    
+    return {
+      promise: promise,
+      resolve: resolve,
+      reject: reject
+    };
+  }
+};
